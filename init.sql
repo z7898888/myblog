e76390cf8c517a195f1148a7b0217c35341a63eb
ppc <ppc@ubuntu>
2017-04-20 07:54:13 +0100

    添加 init.sql 初始化数据库脚本

---
+++ init.sql
@@ -0,0 +1,17 @@
+drop database if exists myblog;
+drop user if exists admin;
+
+create user admin@'%' identified by '123';
+grant select on myblog.* to admin;
+create database myblog;
+
+use myblog; # 使用数据库
+
+create table users(
+		id varchar(50)  primary key,
+		password varchar(50),
+		role enum('Admin','User') default 'User',
+		email varchar(50)
+);
+
+insert into users(id,password) values('dean', 'password1234');
