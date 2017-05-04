3f48b4c762ac5288cb05beba7cebb93cdef70bf9
ppc <ppc@ubuntu>
2017-05-04 14:53:54 +0100

    完善 orm.py 和 db.py 的功能

--- www/db.py
+++ www/db.py
@@ -2,25 +2,37 @@
 #coding:utf-8
 
 import MySQLdb as db;
+import threading;
 
 class DBError(StandardError):
 	pass;
 
 class DBEngine:
+	def __init__(self):
+		self.is_connect = False;
+
 	def connect(self):
-		self.db = db.connect(
+		try:
+			if not self.is_connect:
+				self.db = db.connect(
 				host='localhost',user='admin',passwd='123abc',db='myblog');
-		self.cur = self.db.cursor();
+				self.cur = self.db.cursor();
+				self.is_connect = True;
+		except Exception,e:
+			raise DBError(e);
 	
 	def close(self):
 		if hasattr(self,'cur'):
 			self.cur.close();
+			self.db.close();
 			del self.cur;
+			del self.db;
+			self.is_connect = False;
 	
-	def execute(self, cmd_str):
+	def execute(self, cmd_str, *args):
 		if hasattr(self,'cur'):
 			try:
-				return self.cur.execute(cmd_str);
+				return self.cur.execute(cmd_str, *args);
 			except Exception, e:
 				raise DBError(e);
 
@@ -28,15 +40,47 @@ class DBEngine:
 		if hasattr(self,'cur'):
 			try:
 				ret = self.cur.fetchone();
-				if ret!=None: yield ret;
+				while ret:
+					yield ret;
+					ret = self.cur.fetchone();
 			except Exception,e:
 				raise DBError(e);
+
+class _DBCtx(threading.local):
+	def __enter__(self):
+		self.db = DBEngine();
+		self.db.connect();
+		return self.db;
+
+	def __exit__(self, excyte, excvalue, traceback):
+		self.db.execute('commit');
+		self.db.close();
+
+_db_ctx = _DBCtx();
+
+def connectDB():
+	return _db_ctx;
 
 if __name__ == '__main__':
 	engine = DBEngine();
 	engine.connect();
-	n = engine.execute('select * from users');
-	print '受影响的行数:%d'%n;
+
+	n = engine.execute('select * from users;');
+	print '影响 %d'%n;
 	for u in engine.fetch():
 		print u;
+	
+	n = engine.execute('show databases;');
+	print '影响 %d'%n;
+	for u in engine.fetch():
+		print u;
+
 	engine.close();
+
+	with connectDB() as db:
+		n = db.execute('show tables;');
+		print '影响 %d'%n;
+		for u in db.fetch():
+			print u;
+		raw_input();
+
--- www/orm.py
+++ www/orm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-from db import DBEngine;
+from db import connectDB, DBError;
 
 class ModelMetaclass( type ):
 	def __new__(cls, name, bases, attrs):
@@ -17,6 +17,10 @@ class ModelMetaclass( type ):
 			if '__table__' not in attrs.keys():
 				attrs['__table__'] = name;
 			attrs['__mappings__'] = mappings;
+			attrs['__fields__'] = [
+				(cfn,dfn.fieldName()) for cfn,dfn in mappings.items()];
+			attrs['__pk__'] = [
+				(cfn,dfn) for cfn,dfn in mappings.items() if dfn._pk];
 		return type.__new__(cls, name, bases, attrs);
 
 class FieldModel:
@@ -71,32 +75,113 @@ class TableModel( dict ):
 				cls.__table__, cls.__table__);
 		return sql;
 
+	def _get_pk_fields( self ):
+		attr_list, pk_list = zip(*self.__pk__);
+		pks =  ['%s=%s'%(fn.fieldName(),'%s') for fn in pk_list];
+		args = [getattr(self,cfn,None) for cfn in attr_list];
+		return pks,args;
+
+	def _get_all_fields(self):
+		attr_list, fields = zip(*self.__fields__);
+		args = [getattr(self, fn, None) for fn in self.__mappings__.keys()];
+		return fields,args;
+
 	def insert( self ):
-		field_list = [fd.fieldName() for fd in self.__mappings__.values()];
-		fields = ','.join( field_list );
-		params = ','.join( ('?' for i in field_list) );
-		values = [ getattr(self, field, None) for field in field_list];
+		flds,values = self._get_all_fields();
+		fields = ','.join( flds );
+		params = ','.join( (r'%s' for i in flds) );
 		sql = 'insert into %s(%s) values(%s);'%(
 				self.__table__, fields, params);
-		print values;
-		return sql;
+		return self._execute(sql, False, *values);
 	
 	def delete( self ):
-		pass;
+		pks, args = self._get_pk_fields();
+		sql = 'delete from %s where %s'%(self.__table__,','.join(pks));
+		return self._execute(sql, False, *args);
 
 	def update( self ):
-		pass;
+		pks , args = self._get_pk_fields();
+		fds, vals = self._get_all_fields();
+		fields = ','.join( '%s=%s'%(fd,'%s') for fd in fds);
+		sql = 'update %s set %s where %s'%(
+				self.__table__, fields ,','.join(pks));
+		params = vals+args;
+		return self._execute(sql, False, *params);
 
-	def select( self ):
-		pass;
+	@classmethod
+	def get_all( cls ):
+		attrs, fields = zip(*cls.__fields__);
+
+		sql = 'select %s from %s;'%(','.join(fields),cls.__table__);
+		
+		rcds = cls._execute(sql, True);
+
+		return cls._generate_obj( attrs, rcds );
+
+	@classmethod
+	def get_one( cls , **kw):
+		obj = cls( **kw );
+		pks,args = obj._get_pk_fields();
+		attrs, fds = zip(*cls.__fields__);
+		sql = 'select %s from %s where %s;'%(
+				','.join(fds), cls.__table__,' and '.join(pks));
+
+		rcds = cls._execute(sql, True, *args);
+
+		n = len(rcds);
+		if n == 1: return cls._generate_obj( attrs, rcds )[0];
+		elif n < 1: raise DBError('未查到相关记录');
+		else: raise DBError('查到 %d 条记录'%n);
+
+	@classmethod
+	def select( cls, where, *args ):
+		attrs, fds = zip(*cls.__fields__);
+		sql = 'select %s from %s %s;'%(','.join(fds) ,cls.__table__, where);
+
+		rcds = cls._execute(sql, True, *args);
+
+		return cls._generate_obj(attrs, rcds);
+
+	@classmethod
+	def _generate_obj(cls, attrs, rcd_list):
+		ret = [];
+		for rcd in rcd_list:
+			obj = cls();
+			for field, value in zip(attrs, rcd):
+				setattr(obj,field,value);
+			ret.append(obj);
+		return ret;
+
+	@classmethod
+	def _execute( cls, sql, flag = False, *args ):
+		print sql, flag, args;
+		ret = None;
+		with connectDB() as engine:
+			ret = engine.execute(sql,args);
+			if flag:
+				ret = [ r for r in engine.fetch() ];
+		return ret;
 
 if __name__ == '__main__':
 	class User( TableModel ):
-		__table__ = 'UserInfo';
-		id = FieldModel('id', 'char(50)', primaryKey=True);
-		passwd = FieldModel('passwd', 'char(100)');
-	print User.create();
-	print User.drop();
-	user = User();
-	user.id = 123;
-	print user.insert();
+		__table__ = 'users';
+		id = FieldModel('id', 'varchar(50)', primaryKey=True);
+		passwd = FieldModel('password', 'varchar(50)');
+
+#	user = User(id='sammy',passwd='password4321');
+#	n = user.insert();
+#	print '受到影响的行数:', n;
+	
+	users = User.get_all();
+	for user in users:
+		print user.id, user.passwd;
+
+	print;
+	user = User.get_one(id='sammy');
+	print user;
+	print user.id, user.passwd;
+
+	print;
+	users = User.select( 'where id=%s','sam' );
+	for user in users:
+		print user.id, user.passwd;
